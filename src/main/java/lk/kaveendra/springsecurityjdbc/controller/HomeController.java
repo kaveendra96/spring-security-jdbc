40b5de13c1cc9ef25c583d4502db41bff34c1692
kaveendra96 <kaveendra96@gmail.com>
2020-12-26 01:19:42 +0530

    init project

---
+++ src/main/java/lk/kaveendra/springsecurityjdbc/controller/HomeController.java
@@ -0,0 +1,27 @@
+package lk.kaveendra.springsecurityjdbc.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "home controller";
+    }
+
+    @GetMapping("/user")
+    public String user() {
+        return "user controller";
+    }
+
+    @GetMapping("/admin")
+    public String admin() {
+        return "admin controller";
+    }
+
+
+}
